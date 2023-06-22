9fed3ab6b66b8a054f273e2856f8058f5e4ebae6
Essam Said <100171793+EssamSaid24@users.noreply.github.com>
2023-06-22 23:06:56 +0300

    Connect to mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000;
-app.use(express.static(path.join(__dirname, '../client')));
 
+
+
+app.use(express.static(path.join(__dirname, '../client')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,  '../client/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.post('/',(req,res) => {
+  
+})
+
+mongoose.connect("mongodb+srv://essam:essam@cluster0.keq6tjs.mongodb.net/?retryWrites=true&w=majority")
+  .then( result => {
+    app.listen(port);
+    console.log(`Server running on port ${port}`);
+
+  })
+  .catch( err => {
+    console.log(err);
+  }); 
