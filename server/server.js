769fc2cd32789a203dd929c46a76afea3875bae1
Essam Said <100171793+EssamSaid24@users.noreply.github.com>
2023-06-23 23:11:00 +0300

    Add Task Schema

--- server/server.js
+++ server/server.js
@@ -4,18 +4,20 @@ const app = express();
 const mongoose = require('mongoose');
 const port = 3000;
 
-
+const url_database = "mongodb+srv://essam:essam@cluster0.keq6tjs.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(express.static(path.join(__dirname, '../client')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,  '../client/index.html'));
 });
 
 app.post('/',(req,res) => {
-  
+  let result = res.body;
+  console.log(result)
 })
 
-mongoose.connect("mongodb+srv://essam:essam@cluster0.keq6tjs.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(url_database)
   .then( result => {
     app.listen(port);
     console.log(`Server running on port ${port}`);
